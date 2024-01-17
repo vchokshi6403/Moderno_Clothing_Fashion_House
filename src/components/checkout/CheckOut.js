3d5c82f96edea98d5b8b488cdcf71ebab3ce2026
vchokshi6403 <102212357+vchokshi6403@users.noreply.github.com>
2024-01-17 15:55:03 +0530

    Update CheckOut.js

--- src/components/checkout/CheckOut.js
+++ src/components/checkout/CheckOut.js
@@ -27,7 +27,7 @@ function Checkout() {
 
   var formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "INR",
+    currency: "USD",
 
     // These options are needed to round to whole numbers if that's what you want.
     minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
